9bd725669a2e7f0477c6b06df30978e3923b3d4c
Bouxi <bg23@live.be>
2025-12-01 12:30:46 +0100

    Ajout des pages vitrines (Services, Portfolio, About, Contact) + PageController

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,24 +8,42 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
+    /**
+     * Page Services
+     * URL : /services
+     */
     #[Route('/services', name: 'app_services')]
     public function services(): Response
     {
+        // On retourne simplement le template
         return $this->render('pages/services.html.twig');
     }
 
+    /**
+     * Page Portfolio
+     * URL : /portfolio
+     */
     #[Route('/portfolio', name: 'app_portfolio')]
     public function portfolio(): Response
     {
         return $this->render('pages/portfolio.html.twig');
     }
 
+    /**
+     * Page A propos
+     * URL : /a-propos
+     */
     #[Route('/a-propos', name: 'app_about')]
     public function about(): Response
     {
         return $this->render('pages/about.html.twig');
     }
 
+    /**
+     * Page Contact
+     * (formulaire complet à venir)
+     * URL : /contact
+     */
     #[Route('/contact', name: 'app_contact')]
     public function contact(): Response
     {
